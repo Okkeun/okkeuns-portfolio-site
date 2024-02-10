378603aba7133e7f49fc6668a26c9050d04d36e5
Okkeun <okkeun@yahoo.com>
2024-02-10 15:23:54 +0100

    Add popup windows after clicking the button from project list

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,1 +1,17 @@
 console.log('Siema świsdsfecie!');
+
+
+
+/*
+ZAŁOŻENIA DLA NASZEJ APLIKACJI
+Pojawia się postać, którą możemy wyzwać na pojedynek czarodziejów. 
+
+Pojawiają się 3 przyciski: PAPIER, KAMIEŃ, NOŻYCE.
+Po chwili pojawia się licznik do 3 z odliczaniem. W tym czasie musimy kliknąć przycisk. Komputer też podejmuje decyzję.
+
+Następna faza to RESOLVE - pojawia się animacja pokazująca kto wygrał. Będzie 9 oryginalnych animacji.
+
+Postać wraca na swoje miejsce i pojawia się przycisk następnej rundy.
+
+Po kilku razach gra się kończy, z prostą animacją wygranej lub przegranej.
+*/
