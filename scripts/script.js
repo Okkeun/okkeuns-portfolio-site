7b00868357614187e1d736388fa28a7e621a9d13
Okkeun <okkeun@yahoo.com>
2024-04-12 19:31:00 +0200

    Add a simple script that changes the text after hover

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 updateNumbers();
+hoverSpansAndChangeText();
 
 function updateNumbers () {
     let currentYear = new Date().getFullYear();
     document.querySelector('footer p').textContent = "Copyright: K. 'Okkeun' Okołowski "+"("+currentYear+")";
+}
+
+function hoverSpansAndChangeText() {
+    document.getElementById('nihongo').addEventListener('mouseover', () => {
+        document.getElementById('nihongo').textContent = '日本語の学習'; 
+    });
+    document.getElementById('nihongo').addEventListener('mouseout', () => {
+        document.getElementById('nihongo').textContent = 'Learning Japanese'; 
+    });
 }
