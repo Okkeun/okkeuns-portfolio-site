7d4e37bc45687130ebad9882deef0eb6bd4e26cc
Okkeun <okkeun@yahoo.com>
2024-04-12 19:19:39 +0200

    Arrange content with information about myself and add a script to update the year in the copyright

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,6 @@
-/*disableButtonsWhenPopupIsActive('.rock-paper-scissors-project-button', '#popup-rps-game', '#RPS-take-me-backBTN');
+updateNumbers();
 
-//if pop-up is active, disable buttons
-function disableButtonsWhenPopupIsActive(_button, _popUp, _buttonExit) {
-    //assign the object to a variable
-    var button = document.querySelector(_button);
-    var popUp = document.querySelector(_popUp);
-    var buttonExit = document.querySelector(_buttonExit);
-    //check if mouse will hover
-    popUp.addEventListener('mouseover', function() {
-        button.classList.add('hidden');
-    });
-    //if exit from popup, the button will become active again
-    buttonExit.addEventListener('click', function () {
-        button.classList.remove('hidden');
-    });
-    
-    console.log(button + popUp);
-    //check when button is clicked
+function updateNumbers () {
+    let currentYear = new Date().getFullYear();
+    document.querySelector('footer p').textContent = "Copyright: K. 'Okkeun' Okołowski "+"("+currentYear+")";
 }
-*/
